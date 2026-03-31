100d34b05aee10c25d1eef643aaf19c89f21c1a2
onaolapomay <raphy.mayo@gmail.com>
2026-04-01 07:06:53 +1100

    Fixed the carousel issue

--- script.js
+++ script.js
@@ -9,41 +9,38 @@ hamburger.addEventListener("click", () => {
 
 
 const swiper = new Swiper('.slider-wrapper', {
-    loop: true,
+    loop: false,
+    rewind: true,
     grabCursor: true,
     spaceBetween: 10,
-  
-    // If we need pagination
+
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
         dynamicBullets: true
     },
   
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
 
-    // Responsive breakpoints
     breakpoints: {
-    0: {
-        slidesPerView: 1,
-    },
-    480: {
-        slidesPerView: 2,
-    },
-    768: {
-        slidesPerView: 3,
-    },
-    1024: {
-        slidesPerView: 4,
+        0: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+        480: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        768: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+        }
     }
-}
-  });
-
-
-
-
-  
+});
